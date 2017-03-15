6368f5a9aeee6dc7d6ae51abb71e80eea0ec562e
Richard Beese <richardbeese@Richards-MacBook-Air.local>
2017-03-14 22:36:12 -0600

    Huge success with getting stuff to main page

--- app/components/ArtShow.js
+++ app/components/ArtShow.js
@@ -1,29 +1,29 @@
 import React, { PropTypes } from 'react';
 import { pictureBox, multipleRow } from '../styles/styles.scss';
+import { Row, Col, Card, CardTitle, } from 'react-materialize';
 
 const ArtShow = ({ data }) =>
+    // <div className={multipleRow}>
+    //     <p>{data.name}  {data.price} </p>
+    //     <p>{data.artist}</p>
+    //     <img className={pictureBox} src={data.img}/>
+    // </div>;
     <div className={multipleRow}>
-        <p>{data.name}  {data.price} </p>
-        <p>{data.artist}</p>
-        <img className={pictureBox} src={data.img}/>
+      <Row className={pictureBox}>
+        <Col>
+          <Card header={<CardTitle reveal image={data.img} waves="light"/>}
+            title={data.name}
+            reveal={<p>Here is some more information about this product that is only revealed once clicked on.</p>}>
+            <p><a href="#">This is a link</a></p>
+          </Card>
+        </Col>
+      </Row>
     </div>;
-    // <div>
-    //   <div className="col s12 m4 l4">
-    //       <div className="card">
-    //           <div className="card-image waves-effect waves-block waves-light">
-    //               <img className="activator" src={data.image}/>
-    //           </div>
-    //           <div className="card-content">
-    //               <span className="card-title activator grey-text text-darken-4">Project Title <i className="mdi-navigation-more-vert right">{data.name}</i></span>
-    //               <p><a href="#">Project link</a></p>
-    //           </div>
-    //           <div className="card-reveal">
-    //               <span className="card-title grey-text text-darken-4">Project Title <i className="mdi-navigation-close right"></i></span>
-    //               <p>Here is some more information about this project that is only revealed once clicked on.</p>
-    //           </div>
-    //       </div>
-    //   </div>
-    // </div>; Tara we can use this code onlyl if we can import Materialize
+          // <Card className="small"
+          //   header={<CardTitle image= {{data.image}} > {data.name} </CardTitle>}
+          //   actions={[<a href="#">This is a link</a>]}>
+          //   I am a simple card.  I contain small bits of info.
+          // </Card>
 
 ArtShow.propTypes = {
     data: PropTypes.object
